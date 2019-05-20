d28527063303d00809a7ffd10cbdf317343069ea
bassanodaniele <bassano.daniele@iisgalvanimi.edu.it>
2019-05-20 10:47:52 +0000

    second_commit

--- Front-end/src/app/app.component.ts
+++ Front-end/src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Front-end';
+
+  signUpVisible: boolean = false;
+  signInVisible: boolean = false;
+
+  showSignUp(): boolean {
+    if (this.signUpVisible)
+      this.signUpVisible = false;
+    else this.signUpVisible = true;
+    return false;
+  }
+
+  showSignIn(): boolean {
+    if (this.signInVisible)
+      this.signInVisible = false;
+    else this.signInVisible = true;
+    return false;
+  }
+
 }
